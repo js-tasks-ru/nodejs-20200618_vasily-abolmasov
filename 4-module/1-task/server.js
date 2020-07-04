75d2e8ce643646acdf63615c63085df051384634
Vasiliy Abolmasov <vasily.abolmasov@dalee.ru>
2020-07-04 19:26:26 +0300

    Add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -9,9 +10,36 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  this.return404 = function(message = null) {
+    res.statusCode = 404;
+    res.end(message);
+  };
+
   switch (req.method) {
     case 'GET':
+      try {
+        if (req.url.indexOf('/', 1) >= 0) {
+          console.log(400);
+          res.statusCode = 400;
+          res.end('Subfolders are not supported');
+          return;
+        }
 
+        if (!fs.existsSync(filepath)) {
+          res.statusCode = 404;
+          res.end('not found');
+          return;
+        }
+
+        const readStream = fs.createReadStream(filepath);
+        res.writeHead(200);
+        readStream.pipe(res);
+
+        break;
+      } catch (err) {
+        res.statusCode = 500;
+        res.end('Error');
+      }
       break;
 
     default:

b382a48919f1790a2cdf0b18cd133d624a80ccf3
Vasiliy Abolmasov <vasily.abolmasov@dalee.ru>
2020-07-05 21:51:19 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,21 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (req.url.indexOf('/', 1) >= 0) {
+        res.statusCode = 400;
+        res.end('Subfolders are not supported');
+        return;
+      }
 
+      if (!fs.existsSync(filepath)) {
+        res.statusCode = 404;
+        res.end('File don`t exist');
+        return;
+      }
+
+      fs.unlinkSync(filepath);
+      res.statusCode = 200;
+      res.end('Deleted');
       break;
 
     default:

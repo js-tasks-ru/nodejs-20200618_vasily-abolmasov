9d079129b181139d80484f56b4a77666c9226f24
Vasiliy Abolmasov <vasily.abolmasov@dalee.ru>
2020-09-04 22:15:29 +0300

    add solution for 7-module 3-task fix-4

--- 7-module/3-task/libs/mustBeAuthenticated.js
+++ 7-module/3-task/libs/mustBeAuthenticated.js
@@ -1,7 +1,6 @@
 module.exports = function mustBeAuthenticated(ctx, next) {
   if (!ctx.user) {
     const msg = 'Пользователь не залогинен';
-    console.error(msg);
     ctx.status = 401;
     ctx.body = {error: msg};
     return;

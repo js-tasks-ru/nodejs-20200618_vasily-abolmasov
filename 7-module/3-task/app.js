753ace4f376acb2a9db4ea300fafb6d7c46b868a
Vasiliy Abolmasov <vasily.abolmasov@dalee.ru>
2020-09-04 20:51:45 +0300

    add solution for 7-module 3-task fix-2

--- 7-module/3-task/app.js
+++ 7-module/3-task/app.js
@@ -32,7 +32,7 @@ app.use(async (ctx, next) => {
 app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
-    await Session.create({token: token, user: user, lastVisit: new Date()});
+    Session.create({token: token, user: user, lastVisit: new Date()});
 
     return token;
   };
